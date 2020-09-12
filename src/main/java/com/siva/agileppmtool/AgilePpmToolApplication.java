1f1139dc87477d091fbc5648dbcf1d569a2588f6
sambujanni <siva.janni11@gmail.com>
2020-09-13 00:29:11 +0530

    first commit

---
+++ src/main/java/com/siva/agileppmtool/AgilePpmToolApplication.java
@@ -0,0 +1,13 @@
+package com.siva.agileppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilePpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilePpmToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siva/agileppmtool/AgilePpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.agileppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilePpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
